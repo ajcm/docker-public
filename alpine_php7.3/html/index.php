dae9937743b51d0671369fcd7bd80324c52d4b59
Armando Marques <mail@armandomarques.com>
2020-03-14 18:05:45 +0000

    new files

---
+++ alpine_php7.3/html/index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>alpine/php7.3</title>
+</head>
+
+<body>
+
+<b>alpine/php7.3</b>
+<p>docker image running alpine/php7.3</p>
+
+<p><a href="https://hub.docker.com/repository/docker/ajcm/alpine_php7.3"> image repository</a></p>
+
+
+
+<p><a href="/adminer-4.7.6-en.php"> adminer 4.7.6</a></p>
+<p><a href="/phpinfo.php"> php info</a></p>
+
+</body>
+</html>
