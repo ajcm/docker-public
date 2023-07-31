d0962d0e85ecedf3f969d02ec9c0ac1609f5240d
ajcm <ajcm2020@icloud.com>
2023-07-31 10:52:08 +0200

    new files

--- apache-php-custom/www/index.php
+++ apache-php-custom/www/index.php
@@ -1,18 +1,23 @@
 <html>
 <head>
-    <h5>Test image: apache_php7 </h5>
+    <h5>Base image: php:8-apache </h5>
 </head>
 
 <body>
-<p><a href="/adminer.php">adminer db </a></p>
-<p><a href="/request.php">check requests </a></p>
-<p><a href="/env.php">env variables </a></p>
-<p><a href="/phpinfo.php">php info </a></p>
-<p><a href="/nelson.php">nelson muntz </a></p>
 
 <i>
 <?php echo 'Current PHP version: ' . phpversion(); ?>
 </i>
+
+<p><a href="/request.php">check requests </a></p>
+<p><a href="/env.php">env variables </a></p>
+<p><a href="/phpinfo.php">php info </a></p>
+<p><a href="/adminer-4.8.1.php">adminer 4.8.1</a></p>
+<p><a href="/phpMyAdmin-5.2.1/index.php">php myadmin 5.2.1 </a></p>
+<br/>
+<p><a href="/nelson.php">nelson muntz </a></p>
+
+
 
 </body>
 </html>
