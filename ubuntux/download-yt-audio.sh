84088ddb342317e5ca6da567e273057345bc094a
Armando Marques <mail@armandomarques.com>
2024-12-31 11:19:05 +0000

    remove ux

--- ubuntux/download-yt-audio.sh
+++
@@ -1,7 +0,0 @@
-echo "download ($1) to ($2)"
-
-rm -rf /tmp/*
-yt-dlp --write-thumbnail  --audio-quality 10  -x  --audio-format mp3 -P /tmp  https://www.youtube.com/watch?v=$1 
-detox /tmp/*
-aws s3 sync /tmp s3://$2
-
